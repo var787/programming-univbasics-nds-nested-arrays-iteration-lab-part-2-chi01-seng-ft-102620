81eda89946ad9bd2d0d1c3c2c66196b7e65c7891
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-12 05:57:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,9 @@ def find_min_in_nested_arrays(src)
     i=0
     while element_index<src[row_index].count do
 
-      if src[row_index][element_index]>i
-        i=src[row_index][element_index]
+      i=src[row_index][element_index].minimum
 
-      end
+
 
    element_index+=1
     end
